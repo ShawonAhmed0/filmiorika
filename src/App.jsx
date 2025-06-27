54d887ea69be1c6e6a7caa22dc6c33b52f3b6713
ShawonAhmed0 <shawona145@gmail.com>
2025-06-27 20:13:12 +0600

    second_push

---
+++ src/App.jsx
@@ -0,0 +1,135 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import Search from "./components/search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+import { useDebounce } from "@uidotdev/usehooks";
+import { getTrendingMovies, updateSearchCount } from "./appwrite";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+const App = () => {
+  //Using UseState hooks to get a working search funtion
+  const [searchTerm, setsearchTerm] = useState("");
+
+  //Using UseState hooks to shwowing error message
+  const [errorMessage, seterrorMessage] = useState("");
+
+  const [movieList, setmovieList] = useState([]);
+
+  const [trendingMovies, settrendingMovies] = useState([]);
+
+  const [isLoading, setisLoading] = useState(false);
+
+  const [debouncedSearchTerm, setdebouncedSearchTerm] = useState("");
+
+  useDebounce(() => setdebouncedSearchTerm(searchTerm), 1000, [searchTerm]);
+
+  const fetchMovies = async (query = "") => {
+    setisLoading(true);
+    seterrorMessage("");
+
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to Fetch Movies");
+      }
+
+      const data = await response.json();
+
+      if (data.Response == "False") {
+        seterrorMessage(data.Error || "Failed to fetch Movies");
+        setmovieList([]);
+        return;
+      }
+
+      setmovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+    } catch (error) {
+      console.error(`Error fetching moviews:${error}`);
+      seterrorMessage("Error fetching moviess. Please try again later.");
+    } finally {
+      setisLoading(false);
+    }
+  };
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      settrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+    }
+  };
+  //Using UseEffect hooks to fetch movies when the App loads
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without The Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+        </header>
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
